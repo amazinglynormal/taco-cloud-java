129f553f163188b21bdb9051b3e8866587d71502
Stephen <stephenbrady365@gmail.com>
2021-08-20 20:08:47 +0100

    Add OrderRepository interface

--- src/main/java/sia/tacocloud/data/OrderRepository.java
+++ src/main/java/sia/tacocloud/data/OrderRepository.java
@@ -3,5 +3,8 @@ package sia.tacocloud.data;
 import org.springframework.data.repository.CrudRepository;
 import sia.tacocloud.Order;
 
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<Order, Long> {
+    List<Order> findByDeliveryZip(String deliveryZip);
 }
